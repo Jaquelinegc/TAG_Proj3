c8007aa1fefddf43df554aa0ffe43a6b9e0d9b0a
anapmt <43222877+apmt@users.noreply.github.com>
2019-10-31 09:54:03 -0300

    Update show.cpp

--- show.cpp
+++ show.cpp
@@ -4,26 +4,5 @@ using namespace std;
 /*Mostra na linha de comando os emparelhamentos obtidos e a quantidade
 de professores que puderam ser alocados de forma estavel*/
 void imprimir_resultado (){
-	/*Variaveis auxiliares*/
-	int stable_profs = 0;/*contador para professores alocados de forma estavel*/
-	list<Escola>::iterator escola;
-	escola = lista_escolas.begin();
-	cout<<"----- EMPARELHAMENTOS ------"<<endl;
-	/*Percorre a lista de escolas*/
-	while(escola != lista_escolas.end()){
-		/*Imprime o emparelhamento da primeira vaga*/
-		cout << 'E' << escola->codigo << ' ' << 'P' <<escola->pareamento.front();
-		stable_profs++;
-		/*Imprime o emparelhamento da segunda vaga case ela exista*/
-		if(escola->pareamento.size()>1){
-			cout << ' ' << 'P' << escola->pareamento.back();
-			stable_profs++;
-		}
-		escola++;
-		cout << endl;
-	}
-	/*Imprime a quantidade de professores alocados de forma estavel*/
-	cout<< "--------------------------------------------------" <<endl;
-	cout<<"QUATIDADE DE PROFS ALOCADOS DE FORMA ESTAVEL: "<<stable_profs<<endl;
-	cout<< "--------------------------------------------------" <<endl;
+
 }
