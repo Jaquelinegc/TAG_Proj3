94ca118353bf64dadfc2b82cd5b272a2ed45d74b
Jaqueline <jaq1997@hotmail.com>
2019-11-02 21:52:31 -0300

    Update show.cpp

--- show.cpp
+++ show.cpp
@@ -5,4 +5,16 @@ using namespace std;
 de professores que puderam ser alocados de forma estavel*/
 void imprimir_resultado (){
 
+list<Escola>::iterator it;
+iterator list<escola> = list<escola>.begin ();
+
+while (iterator list<escola> != list<escola>.end()) {
+    cout << iterator->pareamento.front() << ' ';
+    If(itv->pareamento.back() !=itv->pareamento.front()){
+        Cout<<iterator->pareamento.back()}
+    iterator list<escola>++;
 }
+}
+
+// for(list<professor>::iterator it = professor.preferencias.begin(); it != professor.preferencias.end(); it++)
+// for(list<escola>::iterator it = escola.pareamento.begin(); it != escola.pareamento.end(); it++)
